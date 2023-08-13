b20711ad3c3288b6a1227ca201727e8a1f8d5fa2
Ratul Chhibber <ratulchhibber@gmail.com>
2023-08-14 00:01:21 +0530

    * Placeholder SearchFilter

---
+++ src/components/SearchFilter.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SearchFilter = () => {
+  return (
+    <View>
+      <Text>SearchFilter</Text>
+    </View>
+  );
+};
+
+export default SearchFilter;
+
+const styles = StyleSheet.create({});
