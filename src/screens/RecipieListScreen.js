28fd584fda172a41a80bcc4c230945f2b0aa134c
Ratul Chhibber <ratulchhibber@gmail.com>
2023-08-14 00:01:06 +0530

    * Using Header in RecipieListScreen

--- src/screens/RecipieListScreen.js
+++ src/screens/RecipieListScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import Header from "../components/Header";
+import SearchFilter from "../components/SearchFilter";
 
 const RecipieListScreen = () => {
-  return <Text>RecipieListScreen</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1, marginHorizontal: 16 }}>
+      <View>
+        <Header headerText={"Hi, John"} headerIcon={"bell-o"} />
+        <SearchFilter />
+      </View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({});
 
 export default RecipieListScreen;
