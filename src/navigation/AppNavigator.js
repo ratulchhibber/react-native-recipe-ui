646dfd07ef9038d34a952bf2beebfea0d4469e2d
Ratul Chhibber <ratulchhibber@gmail.com>
2023-08-13 21:19:34 +0530

    * RecipieListScreen placeholder with routing from Welcome

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
+import RecipieListScreen from "../screens/RecipieListScreen";
 
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
@@ -10,6 +11,7 @@ const AppNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="RecipieList" component={RecipieListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/RecipieListScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const RecipieListScreen = () => {
+  return <Text>RecipieListScreen</Text>;
+};
+
+export default RecipieListScreen;
