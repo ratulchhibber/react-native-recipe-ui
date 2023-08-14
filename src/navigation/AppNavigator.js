05454dd3cadbe223101ada026b6f949f46df0bcf
Ratul Chhibber <ratulchhibber@gmail.com>
2023-08-14 21:06:20 +0530

    * Adding RecipieDetailsScreen to the Navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import RecipieListScreen from "../screens/RecipieListScreen";
+import RecipieDetailsScreen from "../screens/RecipieDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
@@ -12,6 +13,7 @@ const AppNavigator = () => {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="RecipieList" component={RecipieListScreen} />
+        <Stack.Screen name="RecipieDetail" component={RecipieDetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
